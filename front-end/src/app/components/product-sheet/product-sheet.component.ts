b7ff9ffe501c29b5e53cb4fedcbda9aeecb61a49
Unknown <earyende@hotmail.com>
2018-05-08 17:54:34 +0200

    create components
    create payment, product-sheet and basket screen

---
+++ front-end/src/app/components/product-sheet/product-sheet.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-product-sheet',
+  templateUrl: './product-sheet.component.html',
+  styleUrls: ['./product-sheet.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ProductSheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
