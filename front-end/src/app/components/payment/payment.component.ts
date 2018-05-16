2e8603778d0380abecdefd3b0d8a9ce439e26cc3
Unknown <earyende@hotmail.com>
2018-05-16 23:19:08 +0200

    paiement

--- front-end/src/app/components/payment/payment.component.ts
+++ front-end/src/app/components/payment/payment.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProduitCommandeService } from '../../services/produit-commande.service';
+import { ProduitCommande } from '../../model/model.produit-commande';
+import { User } from '../../model/model.user';
 
 @Component({
   selector: 'app-payment',
@@ -8,12 +11,19 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  produitsCommandes: ProduitCommande[] = [];
+  errorMessage: String;
+  currentUser: User;
+  prixTotal: number;
 
-  constructor(private authService :AuthService,
+  constructor(private produitCommandeService :ProduitCommandeService,
+    private authService :AuthService,
     private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.getPanier();
   }
 
   logOut() {
@@ -25,6 +35,24 @@ export class PaymentComponent implements OnInit {
         error => {
 
         });
+  }
+
+  getPanier(){
+    this.produitCommandeService.getByUser(this.currentUser.id)
+      .subscribe(data => {
+        this.produitsCommandes = data;
+        this.calculPrixTotal();} 
+      )
+      
+  }
+
+  calculPrixTotal(){
+    this.prixTotal = 0;
+    console.log("calcul du prix total");
+    for(let comm of this.produitsCommandes){
+      console.log(comm.quantite);
+      this.prixTotal += comm.produit.prix * comm.quantite;
+    }
   }
 
 }
